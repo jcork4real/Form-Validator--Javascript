6563d79759a4bef1862d91ba0f47859503af079f
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2022-08-31 12:10:58 -0600

    Refactored JS

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/*Pull in form itself, and grab each form input by ID
-=====================================================*/
+//capture inputs for DOM manipulation
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -7,8 +6,7 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 
-/*Functions section. Show input error message
-=============================================*/
+//functions
 function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
@@ -16,40 +14,29 @@ function showError(input, message) {
     small.innerText = message;
 }
 
-/*show success outline
-======================*/
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
 
-/*Add event listener on form for submitting
-===========================================*/
-form.addEventListener('submit', function(e) {
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} Is required`)
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//event listeners
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-   if(username.value === ''){
-        showError(username,'Username is required');
-   }else{
-    showSuccess(username);
-   }
-
-   if(email.value === ''){
-        showError(email,'Email is required');
-   }else{
-    showSuccess(email);
-   }
-
-   if(password.value === ''){
-        showError(password,'Password is required');
-   }else{
-    showSuccess(password);
-   }
-
-   if(password2.value === ''){
-        showError(password2,'Confirmation is required');
-   }else{
-    showSuccess(password2);
-   }
+    checkRequired([username, email, password, password2]);
 
 });
